0fbaf850544e377c0defa89fcec34eafc9f507cc
suraj491 <surajroshan491@gmail.com>
2020-08-11 19:24:37 +0530

    Updates

---
+++ precache-manifest.afaa8afc42d8bb1638f01be1f009435f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "062f5b4dcc9f936cef19453580e39002",
+    "url": "/foodrecipe/index.html"
+  },
+  {
+    "revision": "cfb47bd897c83765fe40",
+    "url": "/foodrecipe/static/css/main.4f2ee4b8.chunk.css"
+  },
+  {
+    "revision": "aae22c7e26d700e0b71c",
+    "url": "/foodrecipe/static/js/2.290a8866.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/foodrecipe/static/js/2.290a8866.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfb47bd897c83765fe40",
+    "url": "/foodrecipe/static/js/main.7db00c0d.chunk.js"
+  },
+  {
+    "revision": "46ff916c1a699dacaafe",
+    "url": "/foodrecipe/static/js/runtime-main.b421b74a.js"
+  }
+]);
